f6236f07da58423c8ff902aa9699215b8c99fc6b
faris808 <fariszama2019@gmail.com>
2025-02-02 19:12:38 +0530

    add logout functionality

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,7 +1,16 @@
 import { Navigate, NavLink, Outlet } from "react-router-dom";
 import { useAuthStore } from "../store";
-import { Layout, Menu, theme } from "antd";
-import Icon from "@ant-design/icons";
+import {
+  Avatar,
+  Badge,
+  Dropdown,
+  Flex,
+  Layout,
+  Menu,
+  Space,
+  theme,
+} from "antd";
+import Icon, { BellFilled } from "@ant-design/icons";
 import { useState } from "react";
 import Logo from "../components/icons/Logo";
 import Home from "../components/icons/Home";
@@ -9,6 +18,8 @@ import { foodIcon } from "../components/icons/FoodIcon";
 import GiftIcon from "../components/icons/GiftIcon";
 import BasketIcon from "../components/icons/BasketIcon";
 import UserIcon from "../components/icons/UserIcon";
+import { useMutation } from "@tanstack/react-query";
+import { logout } from "../http/api";
 const { Header, Content, Footer, Sider } = Layout;
 
 const items = [
@@ -26,7 +37,8 @@ const items = [
     key: "/restaurants",
     icon: <Icon component={foodIcon} />,
     label: <NavLink to="/restaurants">Restaurants</NavLink>,
-  },  {
+  },
+  {
     key: "/products",
     icon: <Icon component={BasketIcon} />,
     label: <NavLink to="/products">Products</NavLink>,
@@ -39,6 +51,17 @@ const items = [
 ];
 
 const Dashboard = () => {
+  const {logout: logoutFromStore } = useAuthStore();
+
+  const { mutate: logoutMutate } = useMutation({
+    mutationKey: ["logout"],
+    mutationFn: logout,
+    onSuccess: async () => {
+      logoutFromStore();
+      return;
+    },
+  });
+
   const [collapsed, setCollapsed] = useState(false);
   const {
     token: { colorBgContainer },
@@ -67,7 +90,40 @@ const Dashboard = () => {
           />
         </Sider>
         <Layout>
-          <Header style={{ padding: 0, background: colorBgContainer }} />
+          <Header
+            style={{
+              paddingLeft: "16px",
+              paddingRight: "16px",
+              background: colorBgContainer,
+            }}
+          >
+            <Flex gap="middle" align="start" justify="space-between">
+              <Badge text="Global" status="success" />
+              <Space size={16}>
+                <Badge dot={true}>
+                  <BellFilled />
+                </Badge>
+                <Dropdown
+                  menu={{
+                    items: [
+                      {
+                        key: "logout",
+                        label: "Logout",
+                        onClick: ()=> logoutMutate,
+                      },
+                    ],
+                  }}
+                  placement="bottomRight"
+                >
+                  <Avatar
+                    style={{ backgroundColor: "#fde3cf", color: "#f56a00" }}
+                  >
+                    U
+                  </Avatar>
+                </Dropdown>
+              </Space>
+            </Flex>
+          </Header>
           <Content style={{ margin: "0 16px" }}>
             <Outlet />
           </Content>
