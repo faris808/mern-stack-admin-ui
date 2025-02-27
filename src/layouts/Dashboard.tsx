75f6d1f60000a0b33a4639215c7fee74f33b11c0
faris808 <fariszama2019@gmail.com>
2025-02-27 16:40:18 +0530

    add active selected menu color

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -95,7 +95,7 @@ const Dashboard = () => {
           </div>
           <Menu
             theme="light"
-            defaultSelectedKeys={["/"]}
+            defaultSelectedKeys={[location.pathname]}
             mode="inline"
             items={items}
           />
